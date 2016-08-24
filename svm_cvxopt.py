e75b279ff61b7c6c8c02e353b518bb5b109f6bf0
blue-chip <galoreinoso@hotmail.com>
2016-08-23 23:07:17 -0400

    Enhanced code readability

--- svm_cvxopt.py
+++ svm_cvxopt.py
@@ -28,11 +28,13 @@ pt2_targetf = np.random.uniform(
     data_range[1],
     2)
 
-slope = (pt2_targetf[1]-pt1_targetf[1])/(pt2_targetf[0]-pt1_targetf[0])
+slope = ((pt2_targetf[1]-pt1_targetf[1])
+         /
+         (pt2_targetf[0]-pt1_targetf[0]))
 y_intercept = pt1_targetf[1] - slope * pt1_targetf[0]
 
 leftmostpt_targetf = [data_range[0], slope*(data_range[0])+y_intercept]
-rightmostpt_targetf = [data_range[1], slope*(data_range[0])+y_intercept]
+rightmostpt_targetf = [data_range[1], slope*(data_range[1])+y_intercept]
 
 
 # Plots the target function
@@ -52,31 +54,39 @@ plt.show()
 plt.pause(0.001)
 
 
-# Generating N training data points
+# Creates N training points, assigns them a label and plots them
+
 N = 20
-x1_input = np.random.uniform(-1, 1, N)
-x2_input = np.random.uniform(-1, 1, N)
 
-y_output = []
+x_values = np.random.uniform(
+    data_range[0],
+    data_range[1],
+    N)
+y_values = np.random.uniform(
+    data_range[0],
+    data_range[1],
+    N)
 
-# Computing the y_output (class or category) from the target function
-for index, each in enumerate(x1_input):
+labels = []
+
+# Computing the labels (class or category) from the target function
+for index, each in enumerate(x_values):
     # Computing the value of x2 if it was on the line
     x2_line = m*(each) + y_intercept
 
     # Checking the value of x2 corresponding to that x1 with the x2 from the
     # target function
     # Assigning a -1 or +1 class to it
-    if x2_input[index] < x2_line:
-        y_output.append(-1.0)
+    if y_values[index] < x2_line:
+        labels.append(-1.0)
     else:
-        y_output.append(1.0)
+        labels.append(1.0)
 
 # Plotting the training points
 plt.scatter(
-    x1_input,
-    x2_input,
-    c=y_output,
+    x_values,
+    y_values,
+    c=labels,
     cmap='bwr',
     alpha=1,
     s=100,
@@ -85,36 +95,36 @@ plt.scatter(
 
 # Combining the data vectors into an X matrix of N vectors of features
 X_svm = np.column_stack((
-    x1_input,
-    x2_input
+    x_values,
+    y_values
     ))
 # Changing the class list into a Numpy array
-y_output = np.array(y_output)
+labels = np.array(labels)
 # Initializing the N x N Gram matrix
 K = np.zeros(
     shape=(
-        len(x1_input),
-        len(x1_input)
+        len(x_values),
+        len(x_values)
         )
     )
 
 # Computing the inner products for each pair of vectors
-for i in range(len(x1_input)):
-    for j in range(len(x1_input)):
+for i in range(len(x_values)):
+    for j in range(len(x_values)):
         K[i, j] = np.dot(X_svm[i], X_svm[j])
 
 # Generating all the matrices and vectors
-P = cvxopt.matrix(np.outer(y_output, y_output) * K)
-q = cvxopt.matrix(np.ones(len(x1_input)) * -1)
+P = cvxopt.matrix(np.outer(labels, labels) * K)
+q = cvxopt.matrix(np.ones(len(x_values)) * -1)
 G = cvxopt.matrix(np.vstack([
-    np.eye(len(x1_input)) * -1,
-    np.eye(len(x1_input))
+    np.eye(len(x_values)) * -1,
+    np.eye(len(x_values))
     ]))
 h = cvxopt.matrix(np.hstack([
-    np.zeros(len(x1_input)),
-    np.ones(len(x1_input)) * 999999999.0
+    np.zeros(len(x_values)),
+    np.ones(len(x_values)) * 999999999.0
     ]))
-A = cvxopt.matrix(y_output, (1, len(x1_input)))
+A = cvxopt.matrix(labels, (1, len(x_values)))
 b = cvxopt.matrix(0.0)
 
 # Solving the QP problem
@@ -131,7 +141,7 @@ ind = np.arange(len(a))[ssv]
 # Select the corresponding alphas a, support vectors sv and class labels sv_y
 a = a[ssv]
 sv = X_svm[ssv]
-sv_y = y_output[ssv]
+sv_y = labels[ssv]
 
 # Plotting the support vectors
 plt.scatter(
